fd6ef0dee7168c4c35736ccc251ec70ca10e5cd1
nadev03 <142786072+nadev03@users.noreply.github.com>
2026-09-13 09:38:55 +0300

    bugfix

--- components/contents/Contents.tsx
+++ components/contents/Contents.tsx
@@ -18,7 +18,7 @@ function Contents({ bookId, contents }: Props) {
   return (
     <ul className="IndexPage__list">
       {contents.map((group: TContentGroup, index: number) => (
-        <ContentGroup bookId={bookId} group={group} index={index} key={index} />
+        <ContentGroup bookId={bookId} group={group} key={index} />
       ))}
 
       <AuthorsLink bookId={bookId} />
